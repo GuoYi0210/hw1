fa3f75bfa728f35d3fceaaeea6360bbc49dad409
GuoYi0210 <a0930270656@gmail.com>
2020-10-15 15:51:04 +0800

    edit

---
+++ Sample3.java
@@ -0,0 +1,11 @@
+package hw1;
+
+public class Sample3 
+{
+	public static void main(String[] args)
+	{
+		System.out.println('A');
+		System.out.println("Åwªï¨Ï¥ÎJava§a!");
+		System.out.println(123);
+	}
+}
